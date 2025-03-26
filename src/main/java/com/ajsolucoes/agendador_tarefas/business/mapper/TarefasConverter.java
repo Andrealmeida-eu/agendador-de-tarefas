4078d53477419fe0265f5c8f29ca4a03f289a476
André Luiz Correia de Almeida <a.luiz.almeida03@outlook.com>
2025-03-26 10:17:22 -0300

    adicionados metodo get por email e por periodo

--- src/main/java/com/ajsolucoes/agendador_tarefas/business/mapper/TarefasConverter.java
+++ src/main/java/com/ajsolucoes/agendador_tarefas/business/mapper/TarefasConverter.java
@@ -5,10 +5,15 @@ import com.ajsolucoes.agendador_tarefas.business.dto.TarefasDTO;
 import com.ajsolucoes.agendador_tarefas.infrastructure.entity.TarefasEntity;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface TarefasConverter {
 
     TarefasEntity paraTarefasEntity(TarefasDTO dto);
 
     TarefasDTO paratarefasDTO(TarefasEntity entity);
+
+    List<TarefasEntity> paraListaTarefasEntity(List<TarefasDTO> dtos);
+    List<TarefasDTO> paraListaTarefasDTO(List<TarefasEntity> entities);
 }
